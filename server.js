0c5cb89867c48ad0f023e6c83421fccccfcfe82d
Bandana Chaulagain <138704329+bandanahub@users.noreply.github.com>
2023-08-15 23:50:09 -0400

    Update server.js

--- server.js
+++ server.js
@@ -4,8 +4,11 @@ const mongoose = require('mongoose');
 const app = express();
 app.use(express.json());
 
-mongoose.connect('mongodb://localhost:27017/bookdb', { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Successfully connected to MongoDB'))
+mongoose.connect('mongodb://localhost:27017/bookdb', { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,
+    useFindAndModify: false 
+})  .then(() => console.log('Successfully connected to MongoDB'))
   .catch(err => console.error('Connection error', err));
 
 const bookSchema = new mongoose.Schema({
